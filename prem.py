176f54c8b3fe5115015b47c2e06e77e0b39bc1fc
Mark Vasfeld <64040409+mvasfeld11@users.noreply.github.com>
2023-01-17 18:52:13 -0600

    edited prem

---
+++ prem.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+print('hello')
+
+'''try:
+    sqliteConnection = sqlite3.connect('prem.db')
+    cursor = sqliteConnection.cursor()
+    print("Database created and Successfully Connected to SQLite")
+
+except sqlite3.Error as error:
+    print("Error while connecting to sqlite", error)
+finally:
+    if sqliteConnection:
+        sqliteConnection.close()
+        print("The SQLite connection is closed")'''
